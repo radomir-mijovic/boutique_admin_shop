760d78a1702f406c2407b7f90316aacac725740c
Radomir Mijovic <batomijovic4@gmail.com>
2021-05-11 14:40:55 +0200

    WhiteNoise update

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from whitenoise import WhiteNoise
 
 app = Flask(__name__, static_folder='static')
 app.config.from_object('config.DevelopmentConfig')
-app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='app/static/')
 
 from app import views, login_views, admin_views
